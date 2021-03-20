460eef25639077069e1fdb9eb7657a85e2c20bfd
Eunju Lee <76032774+Eeun-ju@users.noreply.github.com>
2021-03-20 21:56:15 +0900

    Add files via upload

--- Emd-ARIMA.R
+++ Emd-ARIMA.R
@@ -11,4 +11,10 @@ head(soh_data)
 df = cbind(data,soh_data)
 head(df)
 # 최종 데이터 : df
-
+# SOH 그래프 확인
+soh_list = df[df$time == 0,]$soh
+x = c(1:168)
+plot(x,y = soh_list,
+     xlab = "Cycles", ylab = "SOH",
+     main = "Battery5 SOH")
+abline(h=0.7,lwd=2,col="red") #Threshold - 0.7~0.8
